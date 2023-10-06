c6b5cfbe750ac6ca7b9531b005cede637ed83036
Isaacmathias07 <133376387+Isaacmathias07@users.noreply.github.com>
2023-10-06 12:07:30 -0300

    Add files via upload

---
+++ TRABALHOFRONTEND/cardapio.js
@@ -0,0 +1,29 @@
+function pedido(){
+   var opcao = prompt("Escolha uma opcao de 1 a 5")
+   
+   switch(Number(opcao)){
+   case 1:
+       alert("Você escolheu X-SALADA")
+      break;
+    
+   case 2:
+       alert("Você escolheu X-BACON")
+      break;
+
+   case 3:
+       alert("Você escolheu X-TUDO")
+      break;
+
+   case 4:
+       alert("Você escolheu COCA-COLA")
+      break;
+ 
+   case 5:   
+       alert("Você escolheu Sprite")
+      break; 
+   }
+}
+
+
+
+ 
